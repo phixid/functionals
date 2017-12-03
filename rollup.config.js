6c21bf4f4f20cd2ddabf329e738fee9f31822d7e
Kristof Hermans <kristof.hermans.kh@gmail.com>
2017-12-03 20:37:58 +0100

    documentation and build

--- rollup.config.js
+++ rollup.config.js
@@ -4,18 +4,21 @@ import commonjs from 'rollup-plugin-commonjs';
 const pkg = require('./package');
 
 const input = 'src/index.js';
-const output = 'dist/functionals.js';
-const banner = `//  Functionals v${pkg.version}
-//  https://github.com/phixid/${pkg.name}
-//  (c) 2017-${new Date().getFullYear()} ${pkg.author}
-//  Functionals may be freely distributed under the ${pkg.license} license.`;
+const output = `dist/${pkg.name}.js`;
+const packageName = pkg.name.charAt(0).toUpperCase() + pkg.name.slice(1);
+const banner = `/*  ${packageName} v${pkg.version}
+ *  https://github.com/phixid/${pkg.name}
+ *  (c) 2017-${new Date().getFullYear()} ${pkg.author}
+ *  ${packageName} may be freely distributed under the ${pkg.license} license.
+ */
+`;
 
 const config = {
   input: input,
   output: {
     file: output,
     format: 'es',
-    name: 'Functionals'
+    name: packageName
   },
   banner: banner,
   plugins: [
--- src/box.js
+++ src/box.js
@@ -1,7 +1,12 @@
 /**
- * Box
- * @param value
+ * Box takes a value and 'boxes' it up.
+ * The box a has an API which allows us to see the value inside (inspect), let it be changed (map) or given back (fold).
+ *
+ * Box.map: takes a function parameter, applies the it to the Box'ed value and returns the result in a new Box.
+ * Box.fold: takes a function parameter, applies it to the  Box'ed value and returns the result.
+ * Box.inspect: returns a string-template showing the Box'ed up value.
  */
+
 export const Box = (value) => ({
   map: (f) => Box(f(value)),
   fold: (f) => f(value),
