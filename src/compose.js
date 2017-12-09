b182869e20252bb97dcfc62c857530f97ba46d3d
Kristof Hermans <kristof.hermans.kh@gmail.com>
2017-12-09 16:36:16 +0100

    maintain compose comments

--- src/compose.js
+++ src/compose.js
@@ -1,12 +1,9 @@
 /**
  * composeLeft takes two functions and composes them from left to right.
  * composeRight takes two functions and composes them from right to left.
- * compose is an alias for composeRight.
  *
  * @param func1
  * @param func2
- *
- * The first function applies itself to the application of the second function to one or more arguments.
  */
 
 export const composeLeft = (func1, func2) => (...args) => func2(func1(...args));
