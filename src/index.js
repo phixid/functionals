f08d82d3577dc12a7b175a78388a13439ec27eae
Kristof Hermans <kristof.hermans.kh@gmail.com>
2017-12-10 12:54:38 +0100

    put back pipe

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export { Box } from './box';
 export { LazyBox } from './lazybox';
 export { composeLeft, composeRight } from './compose';
-// export { pipeLeft, pipeRight } from './pipe';
+export { pipeLeft, pipeRight } from './pipe';
