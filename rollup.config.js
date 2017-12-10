454273e6df94d9648092539d7d835805254b73b4
Kristof Hermans <kristof.hermans.kh@gmail.com>
2017-12-10 11:04:51 +0100

    CommonJS and ES builds supplied

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,10 @@ import commonjs from 'rollup-plugin-commonjs';
 const pkg = require('./package');
 
 const input = 'src/index.js';
-const output = `dist/${pkg.name}.js`;
+const output = {
+  es: `es/${pkg.name}.js`,
+  cjs: `dist/${pkg.name}.js`
+};
 const packageName = pkg.name.charAt(0).toUpperCase() + pkg.name.slice(1);
 const banner = `/*  ${packageName} v${pkg.version}
  *  https://github.com/phixid/${pkg.name}
@@ -15,13 +18,25 @@ const banner = `/*  ${packageName} v${pkg.version}
 
 const config = {
   input: input,
-  output: {
-    file: output,
-    format: 'es',
-    name: packageName
-  },
+  output: {},
   banner: banner,
   plugins: [resolve(), commonjs()]
 };
+
+if (process.env.NODE_ENV === 'ES') {
+  config.output = {
+    file: output.es,
+    format: 'es',
+    name: packageName
+  };
+}
+
+if (process.env.NODE_ENV === 'CJS') {
+  config.output = {
+    file: output.cjs,
+    format: 'cjs',
+    name: packageName
+  };
+}
 
 module.exports = config;
